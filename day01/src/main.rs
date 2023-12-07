3e57572d7bb2cd1068f5813641bdffffc7124567
Bostjan Meglic <103102696+bmeglic@users.noreply.github.com>
2023-12-07 17:13:01 +0100

    Add day 1 part 2

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,11 +1,7 @@
-fn main() {
-    //let input = include_str!("../input_example.txt");
-    let input = include_str!("../input.txt");
-
+fn part1(input: &str) -> u32 {
     let sum: u32 = input
         .lines()
         .map(|line| {
-
             let first = line.find(|c: char| c.is_ascii_digit());
             let last = line.rfind(|c: char| c.is_ascii_digit());
 
@@ -29,6 +25,50 @@ fn main() {
             (first * 10) + last
         })
         .sum();
+
+    sum
+}
+
+fn part2(input: &str) -> u32 {
+    let words = [
+        "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
+
+    let sum: u32 = input
+        .lines()
+        .map(|line| {
+            let mut digits: Vec<u32> = Vec::new();
+
+            for idx in 0..line.len() {
+                let new_line = &line[idx..];
+
+                if let Some(digit) = new_line.chars().next().unwrap().to_digit(10) {
+                    digits.push(digit);
+                } else {
+                    for word in 0..words.len() {
+                        if new_line.starts_with(words[word]) {
+                            digits.push(word as u32);
+                        }
+                    }
+                }
+            }
+
+            let first = digits.first().unwrap();
+            let last = digits.last().unwrap();
+
+            (first * 10) + last
+        })
+        .sum();
+
+    sum
+}
+
+fn main() {
+    //let input = include_str!("../input_example2.txt");
+    let input = include_str!("../input.txt");
+
+    //let sum = part1(input);
+    let sum = part2(input);
 
     println!("Sum: {sum}");
 }
