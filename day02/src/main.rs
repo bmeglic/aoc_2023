c2183360041bfc095470a7cdb0648176b4d9fb4a
Bostjan Meglic <103102696+bmeglic@users.noreply.github.com>
2023-12-08 17:15:23 +0100

    Add day 2 part 2

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -2,7 +2,7 @@ use std::iter::Sum;
 use std::ops::AddAssign;
 use std::str::FromStr;
 
-#[derive(Debug)]
+#[derive(Debug, Default)]
 struct GameSet {
     red: u32,
     green: u32,
@@ -99,10 +99,8 @@ impl FromStr for Game {
             .expect("Could not get game id")
             .parse::<u32>()?;
 
-        let sets: Result<Vec<GameSet>, anyhow::Error> = part_sets
-            .split("; ")
-            .map(|s| GameSet::from_str(s))
-            .collect();
+        let sets: Result<Vec<GameSet>, anyhow::Error> =
+            part_sets.split("; ").map(GameSet::from_str).collect();
         let sets = sets?;
 
         Ok(Game { id, sets })
@@ -116,8 +114,7 @@ fn part1(input: &str) -> u32 {
         blue: 14,
     };
 
-    let games: Result<Vec<Game>, anyhow::Error> =
-        input.lines().map(|l| Game::from_str(l)).collect();
+    let games: Result<Vec<Game>, anyhow::Error> = input.lines().map(Game::from_str).collect();
 
     let mut total = 0;
 
@@ -142,15 +139,32 @@ fn part1(input: &str) -> u32 {
     total
 }
 
-fn _part2(_input: &str) -> u32 {
-    todo!();
+fn part2(input: &str) -> u32 {
+    let games: Result<Vec<Game>, anyhow::Error> = input.lines().map(Game::from_str).collect();
+
+    let mut total = 0;
+
+    for game in games.unwrap() {
+        let max_game = game
+            .sets
+            .iter()
+            .fold(GameSet::default(), |acc, game_set| GameSet {
+                red: acc.red.max(game_set.red),
+                green: acc.green.max(game_set.green),
+                blue: acc.blue.max(game_set.blue),
+            });
+
+        total += max_game.red * max_game.green * max_game.blue;
+    }
+
+    total
 }
 
 fn main() {
     let input = include_str!("../input.txt");
 
-    let result = part1(input);
-    //let result = part2(input);
+    //let result = part1(input);
+    let result = part2(input);
 
     println!("Result: {result}");
 }
@@ -163,4 +177,14 @@ fn test_part1() {
     Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
     Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
     assert_eq!(part1(&input), 8);
+}
+
+#[test]
+fn test_part2() {
+    let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+    Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+    Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+    Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+    Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+    assert_eq!(part2(&input), 2286);
 }
