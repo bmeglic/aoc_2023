b200f76fbaed48fadd00bd000a638e9fdcf3b39c
Bostjan Meglic <103102696+bmeglic@users.noreply.github.com>
2023-12-13 18:41:24 +0100

    Add day 4 part 2

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -2,7 +2,7 @@ use std::collections::HashSet;
 
 #[derive(Debug, Default)]
 struct Card {
-    id: u32,
+    _id: u32,
     num_win: HashSet<u32>,
     num_own: HashSet<u32>,
 }
@@ -31,7 +31,7 @@ fn parse(input: &str) -> Vec<Card> {
                 .collect();
 
             Card {
-                id,
+                _id: id,
                 num_win,
                 num_own,
             }
@@ -55,11 +55,34 @@ fn part1(input: &str) -> u32 {
         .sum::<u32>()
 }
 
+fn part2(input: &str) -> u32 {
+    let games = parse(input);
+
+    let winning_count: Vec<u32> = games
+        .iter()
+        .map(|card| card.num_win.intersection(&card.num_own).count() as u32)
+        .collect();
+
+    winning_count
+        .iter()
+        .enumerate()
+        .scan(vec![1; winning_count.len()], |counts, (idx, count)| {
+            let num_cards = counts[idx];
+            for i in idx + 1..=idx + *count as usize {
+                counts[i] += num_cards;
+            }
+            Some(num_cards)
+        })
+        .sum()
+}
+
 fn main() {
     let input = include_str!("../input.txt");
 
     let result = part1(input);
     println!("Result part 1: {result}");
+    let result = part2(input);
+    println!("Result part 2: {result}");
 }
 
 #[test]
@@ -70,5 +93,16 @@ Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
 Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
 Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
-    assert_eq!(part1(&input), 13);
+    assert_eq!(part1(input), 13);
+}
+
+#[test]
+fn test_part2() {
+    let input = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
+    assert_eq!(part2(input), 30);
 }
