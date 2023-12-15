96e6b4abbd20e644664afc592988b36dd2e5b2b8
Bostjan Meglic <103102696+bmeglic@users.noreply.github.com>
2023-12-15 18:48:22 +0100

    Add day 5 part 2

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -57,7 +57,6 @@ fn part1(input: &str) -> u64 {
     seeds
         .iter()
         .map(|seed| {
-
             let result = mappings.iter().fold(*seed, |acc, mapping| {
                 for map in mapping {
                     if let Some(result) = map.translate(acc) {
@@ -72,11 +71,49 @@ fn part1(input: &str) -> u64 {
         .expect("There was an error finding out minimum")
 }
 
+fn part2(input: &str) -> u64 {
+    let (_input, (seeds, mappings)) = parse_input(input).expect("Unable to parse input");
+
+    let seeds_ranges: Vec<_> = seeds
+        .chunks(2)
+        .map(|v| Range {
+            start: v[0],
+            end: v[0] + v[1],
+        })
+        .collect();
+
+    seeds_ranges
+        .iter()
+        .map(|seed_mapping| {
+            seed_mapping
+                .clone()
+                .into_iter()
+                .map(|seed| {
+                    let result = mappings.iter().fold(seed, |acc, mapping| {
+                        for map in mapping {
+                            if let Some(result) = map.translate(acc) {
+                                return result;
+                            }
+                        }
+                        acc
+                    });
+                    result
+                })
+                .min()
+                .expect("There was an error finding out minimum")
+        })
+        .min()
+        .expect("There was an error finding out minimum")
+}
+
 fn main() {
     let input = include_str!("../input.txt");
 
     let result = part1(input);
     println!("Result part 1: {result}");
+
+    let result = part2(input);
+    println!("Result part 2: {result}");
 }
 
 #[test]
@@ -116,4 +153,43 @@ humidity-to-location map:
 56 93 4
 ";
     assert_eq!(part1(input), 35);
+}
+
+#[test]
+fn test_part2() {
+    let input = "seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4
+";
+    assert_eq!(part2(input), 46);
 }
